2d9c722b8f63e6bde531e6123a929584044999df
ranamiah221 <ranarasul21@gmail.com>
2024-10-29 10:35:26 +0600

    added register form in register pages

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>hello from register</h2>
+  return (
+    <div className="flex justify-center my-5">
+      <div className="flex bg-purple-200 flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 dark:text-gray-800">
+        <div className="mb-8 text-center">
+          <h1 className="my-3 text-4xl font-bold">Register</h1>
         </div>
-    );
+        <form noValidate="" action="" className="space-y-12">
+          <div className="space-y-4">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="leroy@jenkins.com"
+                className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
+              />
+            </div>
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="password" className="text-sm">
+                  Password
+                </label>
+                <a
+                  rel="noopener noreferrer"
+                  href="#"
+                  className="text-xs hover:underline dark:text-gray-600"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="*****"
+                className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
+              />
+            </div>
+          </div>
+          <div className="space-y-2">
+            <div>
+              <button
+                type="button"
+                className="w-full bg-purple-400 hover:bg-purple-600 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
+              >
+                Register
+              </button>
+            </div>
+            <p className="px-6 text-sm text-center dark:text-gray-600">
+              Don't have an account yet?
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                className="hover:underline dark:text-violet-600"
+              >
+                Register
+              </a>
+              .
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
